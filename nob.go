b171ec10aa4aae82e52c56f3e4073eba212c5244
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-10-23 16:13:00 +0200

    Get html from endpoint

--- nob.go
+++ nob.go
@@ -1,15 +1,43 @@
 package main
 
 import (
+	"fmt"
 	"github.com/urfave/cli"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 )
+
+var endpoint = "https://ordbok.uib.no/perl/ordbok.cgi?OPP="
+
+func getExplanation(word string) string {
+	url := endpoint + word
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return string(body)
+}
 
 func main() {
 	app := cli.NewApp()
 	app.Name = "nob"
 	app.Usage = "get explanation for words"
+
+	app.Action = func(c *cli.Context) error {
+		word := c.Args().Get(0)
+		explanation := getExplanation(word)
+		fmt.Println(explanation)
+		return nil
+	}
 
 	err := app.Run(os.Args)
 	if err != nil {
