502ed494d282270062105d1a42f13264aa82082a
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-10-23 22:33:21 +0200

    Try to parse horrible html structure with XPath query

--- nob.go
+++ nob.go
@@ -2,31 +2,43 @@ package main
 
 import (
 	"fmt"
+	"github.com/antchfx/htmlquery"
 	"github.com/urfave/cli"
-	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
 )
 
-var endpoint = "https://ordbok.uib.no/perl/ordbok.cgi?OPP="
+const ENDPOINT = "https://ordbok.uib.no/perl/ordbok.cgi?OPP="
 
-func getExplanation(word string) string {
-	url := endpoint + word
-	resp, err := http.Get(url)
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer resp.Body.Close()
+func getExplanation(word string) {
 
-	body, err := ioutil.ReadAll(resp.Body)
+	url := ENDPOINT + word
+	doc, err := htmlquery.LoadURL(url)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	// TODO: Parse body
+	for _, n := range htmlquery.Find(doc, `//*[@id="byttutBM"]/tbody/tr[2]/td[2]/div/div[2]`) {
+		//*[@id="art22888"]/div[2]
+		//*[@id="art22723"]/div[2]
+		//*[@id="art17346"]/div[2]
 
-	return string(body)
+
+		//*[@id="byttutBM"]/tbody
+
+
+		//*[@id="byttutBM"]/tbody/tr[2]/td[2]
+
+
+		a := htmlquery.FindOne(n, "//span[2]")
+		fmt.Printf("%s\n", htmlquery.InnerText(a))
+
+		//*[@id="art22892"]/div[2]/span[5]/div[1]/text()
+		//*[@id="art22892"]/div[2]/span[5]/div[1]/div/span[1]/span/text()
+		//*[@id="art22892"]/div[2]/span[5]/div[1]
+		b := htmlquery.FindOne(n, "//span[6]/div")
+		fmt.Printf("%s\n", htmlquery.InnerText(b))
+	}
 }
 
 func main() {
@@ -36,8 +48,7 @@ func main() {
 
 	app.Action = func(c *cli.Context) error {
 		word := c.Args().Get(0)
-		explanation := getExplanation(word)
-		fmt.Println(explanation)
+		getExplanation(word)
 		return nil
 	}
 
